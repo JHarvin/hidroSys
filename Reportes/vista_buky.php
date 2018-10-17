2fe152292a364115c7bc80f33ad7b281e78210db
GersonDuran <32017747+GersonDuran@users.noreply.github.com>
2018-10-17 14:27:36 -0600

    buky
    bky

---
+++ Reportes/vista_buky.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of vista_buky
+ *
+ * @author jerso
+ */
+class vista_buky {
+    //put your code here
+}
